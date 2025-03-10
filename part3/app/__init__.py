37a0eda2650e09188a1f59e1bb63689dd16b52a8
matteof7 <matteofoti741@gmail.com>
2025-03-10 15:23:10 +0100

    modification app/__init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager
 from .api.v1.users import api as users_ns
 from .api.v1.amenities import api as amenities_ns
 from .api.v1.places import api as places_ns
 from .api.v1.reviews import api as reviews_ns
+from .config import Config
 
-def create_app():
+# Initialiser JWTManager en dehors de la fonction pour pouvoir l'importer ailleurs
+jwt = JWTManager()
+
+def create_app(config_class=Config):
     app = Flask(__name__)
+    
+    # Charger la configuration
+    app.config.from_object(config_class)
+    
+    # Initialiser JWT
+    jwt.init_app(app)
+    
     api = Api(
         app, 
         version='1.0', 
