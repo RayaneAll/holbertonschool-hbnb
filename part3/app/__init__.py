1fd7eb6e1b311776a84ae22499c32cafa3431883
matteof7 <matteofoti741@gmail.com>
2025-03-12 12:31:41 +0100

    modification des codes dans auth.py et init.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -28,11 +28,13 @@ def create_app(config_class=Config):
     from .api.v1.amenities import api as amenities_ns
     from .api.v1.places import api as places_ns
     from .api.v1.reviews import api as reviews_ns
+    from .api.v1.auth import api as auth_ns  # Nouveau namespace pour l'authentification
     
     # Register namespaces
     api.add_namespace(users_ns)
     api.add_namespace(amenities_ns)
     api.add_namespace(places_ns)
     api.add_namespace(reviews_ns)
+    api.add_namespace(auth_ns)  # Enregistrement du namespace d'authentification
     
     return app
