e5041dc83e55c7c460eaadd806806a1641850356
RayaneAll <9975@holbertonstudents.com>
2025-03-12 13:16:52 +0100

    Add new features for JWT Authentication : - Import JWTManager from flask_jwt_extended - Create a JWTManager instance - Initialize JWTManager in create_app() - Make sure JWT_SECRET_KEY is defined in the configuration

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager  # Import du JWTManager
 from .api.v1.users import api as users_ns
 from .api.v1.amenities import api as amenities_ns
 from .api.v1.places import api as places_ns
 from .api.v1.reviews import api as reviews_ns
 
+jwt = JWTManager()  # Création de l'instance JWT
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)  # Charger la configuration
+
+    # Ajout de la clé secrète pour JWT (Assurez-vous que DevelopmentConfig la définit)
+    app.config["JWT_SECRET_KEY"] = "super-secret-key"  # À remplacer par une vraie clé sécurisée
+
+    jwt.init_app(app)  # Initialiser JWT avec l'application Flask
 
     api = Api(
         app, 
